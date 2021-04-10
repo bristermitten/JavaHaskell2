a72898743b75a7c9ec9dcf2a8368a9b96083321a
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-04-10 22:13:23 +0100

    Migrate all functionality into haskell because why not

--- Haskell/Calc.hs
+++ Haskell/Calc.hs
@@ -1,7 +1,24 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
+
 module Calc where
-    
-add x y = x + y -- Very simple function to add 2 numbers
 
+import Data.String
+import Foreign.C
 
-foreign export ccall add :: Int -> Int -> Int -- export add as a C header so we can access it from JNI
+parseInput :: String -> [Int]
+parseInput i = parts $ words i
+    where parts [a, p, x] = [read a, read x]
+          parts _ = []
+
+process :: IO String
+process = do
+  putStrLn "Please enter a string in the format <a + b>"
+  i <- getLine
+  let res = parseInput i
+  case res of 
+    [a, b] -> return $ show $ a + b
+    _ -> return "Invalid input"
+
+cProcess = process >>= newCString
+
+foreign export ccall cProcess :: IO CString -- export add as a C header so we can access it from JNI
--- Java/Library.java
+++ Java/Library.java
@@ -1,27 +1,15 @@
-import java.util.Scanner;
 
 public class Library {
     static {
-        System.loadLibrary("calc");
+        System.loadLibrary("calc"); // Loads our JNI library
     }
 
     public static void main(String[] args) {
+        var res = runHS();
 
-        try (var scanner = new Scanner(System.in)) {
-            System.out.println("enter a string in format <a + b>");
-            var exprParts = scanner.nextLine().split("\\s\\+\\s");
-            int a = Integer.parseInt(exprParts[0]);
-            int b = Integer.parseInt(exprParts[1]);
-            int res = addHs(a, b);
-            System.out.println("Result is: " + res);
-        }
+        System.out.println("Result is: " + res);
     }
 
-    /**
-     * This is implemented 
-     * @param a
-     * @param b
-     * @return
-     */
-    public static native int addHs(int a, int b);
+    // Native function that calls haskell function
+    public static native String runHS();
 }
