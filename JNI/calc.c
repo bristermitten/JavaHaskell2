08f252b64e7a909ac3dd0046781c90c12d8b3c0c
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-04-10 22:32:38 +0100

    Fix potential memory leak

--- JNI/calc.c
+++ JNI/calc.c
@@ -1,7 +1,7 @@
 #include "../Haskell/Calc_stub.h" //Provides an explicit binding to add()
+#include "Library.h"
 #include <HsFFI.h>
 #include <jni.h>
-#include "Library.h"
 #include <stdlib.h>
 #include <string.h>
 
@@ -16,5 +16,7 @@ JNIEXPORT jstring JNICALL Java_Library_runHS(JNIEnv *env, jclass clazz) {
   char *buf = (char *)malloc(len);
   strcpy(buf, res);
   free(res);
-  return (*env)->NewStringUTF(env, buf);
+  jstring jstr = (*env)->NewStringUTF(env, buf);
+  free(buf);
+  return jstr;
 }
