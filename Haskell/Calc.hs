3a0c1236b41369191302003f0d078522c1072246
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-04-10 22:32:33 +0100

    Add some Haskell noise

--- Haskell/Calc.hs
+++ Haskell/Calc.hs
@@ -4,21 +4,31 @@ module Calc where
 
 import Data.String
 import Foreign.C
+import Control.Monad
 
 parseInput :: String -> [Int]
 parseInput i = parts $ words i
-    where parts [a, p, x] = [read a, read x]
-          parts _ = []
+  where
+    parts [a, p, x] = [read a, read x]
+    parts _ = []
 
 process :: IO String
 process = do
   putStrLn "Please enter a string in the format <a + b>"
   i <- getLine
   let res = parseInput i
-  case res of 
+  case res of
     [a, b] -> return $ show $ a + b
     _ -> return "Invalid input"
 
 cProcess = process >>= newCString
 
 foreign export ccall cProcess :: IO CString -- export add as a C header so we can access it from JNI
+
+-- These functions are totally useless and unused, but i want this to be marked as a haskell repo on github :D
+
+main = do
+  quantity <- readLn :: IO Int
+  let out = map (* 2) $ filter even [1..quantity]
+  let res = take quantity out -- lol
+  print $ replicateM quantity res
